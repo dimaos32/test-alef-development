9510b3cd29119b0eca92b3bc27186dfd860daabd
Dima Os <dimaos@protonmail.com>
2021-05-29 16:52:03 +0300

    ВЫполняет мобильное меню

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 import {ieFix} from './utils/ie-fix';
 import {iosVhFix} from './utils/ios-vh-fix';
 
-// import {initModals} from './modules/init-modals';
+import {initMobileMenu} from './modules/init-mobile-menu';
 import {renderCards} from './modules/render-cards';
 
 // Utils
@@ -13,5 +13,5 @@ iosVhFix();
 // Modules
 // ---------------------------------
 
-// initModals();
+initMobileMenu();
 renderCards();
---
+++ source/js/modules/init-mobile-menu.js
@@ -0,0 +1,54 @@
+import {disableScrolling, enableScrolling} from './../utils/scroll-lock';
+
+const nav = document.querySelector('.main-nav');
+
+const initMobileMenu = () => {
+  if (!nav) {
+    return;
+  }
+
+  const btn = nav.querySelector('.main-nav__btn');
+  const overlay = nav.querySelector('.main-nav__overlay');
+
+  const onOverlayClick = () => {
+    closeMenu();
+  };
+
+  const onEscPress = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+
+      closeMenu();
+    }
+  };
+
+  const openMenu = () => {
+    nav.classList.add('main-nav--opened');
+
+    disableScrolling();
+
+    overlay.addEventListener('click', onOverlayClick);
+    document.addEventListener('keydown', onEscPress);
+  };
+
+  const closeMenu = () => {
+    nav.classList.remove('main-nav--opened');
+
+    setTimeout(enableScrolling, 300);
+
+    overlay.removeEventListener('click', onOverlayClick);
+    document.removeEventListener('keydown', onEscPress);
+  };
+
+  const toggleMenu = () => {
+    if (nav.classList.contains('main-nav--opened')) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  };
+
+  btn.addEventListener('click', toggleMenu);
+}
+
+export {initMobileMenu};
